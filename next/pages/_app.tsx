f554e3b153bbcadc3b24f50542ee795c3e456331
Karel <karel789@hotmail.com>
2022-03-31 13:16:11 +0300

    front end pages added

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const name = "Ethereum donations";
+  const description = "Ethereum donations for creators";
+  const url = "https://ethdon.xyz";
+  const image = `${url}/favicon.png`;
+
+  return (
+    <>
+      <Head>
+        <title>{name}</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content="ethereum,donations,streaming,streamers,eth,erc20,gaming" />
+
+        <meta name="og:title" content={name} />
+        <meta name="og:url" content={url} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content={image} />
+
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={name} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
