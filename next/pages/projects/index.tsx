d1f66d6e4b3f33a2cbe53583a9f5b9ab062dc8d8
Karel <karel789@hotmail.com>
2022-03-31 13:25:54 +0300

    front end pages

---
+++ next/pages/projects/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <main>
+        <h1>Projects</h1>
+      </main>
+    </Layout>
+  );
+}
