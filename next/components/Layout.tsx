e07a3964d0d671c3d5a5ae6b8e71bf7ad8aa57a6
Karel <karel789@hotmail.com>
2022-04-02 21:50:26 +0300

    changed headerto dynamic import because first load js was 400kB bigger because of it

--- next/components/Layout.tsx
+++ next/components/Layout.tsx
@@ -1,6 +1,7 @@
+import dynamic from "next/dynamic";
 import React from "react";
 import Footer from "./Footer";
-import Header from "./Header";
+const Header = dynamic(() => import('./Header'))
 
 export default function Layout({ children }: { children: any }) {
   return (
